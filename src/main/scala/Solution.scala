e6df21b2c0497289917793f202e06a68afd94502
flx777 <subtirica.florin29@gmail.com>
2023-03-24 13:45:16 +0200

    Tasks 1-3 works

--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -7,19 +7,48 @@ object Solution {
 
   // prerequisites
   def fromStringPPM(image: List[Char]): Image = {
-    
+    val lines = (image.mkString split '\n').toList
+    val header = lines take 3
+    val pixels = lines drop 3 flatMap(
+      _.trim.split("\\s+").map(_.toInt)
+    )
+
+    val dimensions = header(1).split(' ').map(_.toInt) // width and height
+
+    (pixels grouped 3).toList.map(chunk => Pixel(chunk.head, chunk(1), chunk(2))).grouped(dimensions.head).toList
   }
 
-  def toStringPPM(image: Image): List[Char] = ???
+  def toStringPPM(image: Image): List[Char] = {
+    ("P3\n" // PPM format
+      + s"${image.head.length}" // width
+      + " "
+      + s"${image.length}\n" // height
+      + "255\n"
+      :: image
+        .flatten
+        .map(
+          p => s"${p.red} ${p.green} ${p.blue}\n"
+        )
+      ).mkString.toList
+  }
 
   // ex 1
-  def verticalConcat(image1: Image, image2: Image): Image = ???
+  def verticalConcat(image1: Image, image2: Image): Image =
+    image1 ++ image2
 
   // ex 2
-  def horizontalConcat(image1: Image, image2: Image): Image = ???
+  def horizontalConcat(image1: Image, image2: Image): Image =
+    for (tuple <- image1 zip image2) yield tuple._1 ++ tuple._2
+
 
   // ex 3
-  def rotate(image: Image, degrees: Integer): Image = ???
+  def rotate(image: Image, degrees: Integer): Image =
+    (degrees % 360) / 90 match {
+      case 0 => image
+      case 1 => image.transpose.reverse
+      case 2 => image.reverse.map(_.reverse)
+      case 3 => image.reverse.transpose
+    }
 
   // ex 4
   val gaussianBlurKernel: GrayscaleImage = List[List[Double]](
@@ -42,9 +71,12 @@ object Solution {
     List(-1,-2,-1)
   )
 
-  def edgeDetection(image: Image, threshold : Double): Image = ???
+  def edgeDetection(image: Image, threshold : Double): Image = {
 
-  def applyConvolution(image: GrayscaleImage, kernel: GrayscaleImage) : GrayscaleImage = ???
+  }
+
+  def applyConvolution(image: GrayscaleImage, kernel: GrayscaleImage) : GrayscaleImage = {
+  }
 
   // ex 5
   def moduloPascal(m: Integer, funct: Integer => Pixel, size: Integer): Image = ???
